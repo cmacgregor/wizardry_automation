89ae6a323b341109a266a05a3c8d39049792b4b4
Connor MacGregor <macgregoco@gmail.com>
2025-12-15 14:53:22 -0600

    Add critical Chrome arguments for containerized environments
    Chrome needs additional flags to run properly in Docker containers:
    - --headless=new: Use new headless mode (Chrome 109+)
    - --disable-gpu: Disable GPU hardware acceleration
    - --disable-software-rasterizer: Prevent software rendering issues
    - --disable-extensions: Reduce resource usage
    
    These flags prevent Chrome from crashing on startup in containerized
    environments with limited capabilities and no display.
    
    Fixes: "Chrome instance exited" error in containers

--- wizardry_bot.py
+++ wizardry_bot.py
@@ -46,10 +46,16 @@ class WizardryBot:
         options = webdriver.ChromeOptions()
 
         if self.config.get('headless', False):
-            options.add_argument('--headless')
+            options.add_argument('--headless=new')  # Use new headless mode
 
+        # Essential arguments for Docker/containerized environments
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-software-rasterizer')
+        options.add_argument('--disable-extensions')
+
+        # Anti-detection options
         options.add_argument('--disable-blink-features=AutomationControlled')
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
